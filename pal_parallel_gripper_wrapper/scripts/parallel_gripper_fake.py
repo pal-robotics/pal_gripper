abe1ceea3e0765b513d1a5f537e46957216762a0
Sam Pfeiffer <sam.pfeiffer@pal-robotics.com>
2016-06-13 12:25:48 +0200

    Tested version

--- pal_parallel_gripper_wrapper/scripts/parallel_gripper_fake.py
+++ pal_parallel_gripper_wrapper/scripts/parallel_gripper_fake.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 """Action server wrapping
 the real follow joint trajectory action server
@@ -31,11 +31,13 @@ class WrappedFJTA(object):
             exit(0)
         self.real_joint_names = rospy.get_param("~real_joint_names", None)
         if not self.real_joint_names:
-            rospy.logerr("No real joint names given in param: ~real_joint_names")
+            rospy.logerr(
+                "No real joint names given in param: ~real_joint_names")
             exit(0)
         self.gripper_joint_name = rospy.get_param("~gripper_joint_name", None)
         if not self.gripper_joint_name:
-            rospy.logerr("No gripper joint name given in param: ~gripper_joint_name")
+            rospy.logerr(
+                "No gripper joint name given in param: ~gripper_joint_name")
             exit(0)
 
         self._cmd_sub = rospy.Subscriber('/' + self.controller_name + '_controller/command',
@@ -59,8 +61,8 @@ class WrappedFJTA(object):
         rospy.loginfo("At a rate of " + str(self.pub_rate))
 
         self._cmd_pub = rospy.Publisher('/' + self.real_controller_name + '_controller/command',
-                                         JointTrajectory,
-                                         queue_size=5)
+                                        JointTrajectory,
+                                        queue_size=5)
         rospy.loginfo("Publishing to " + self._cmd_pub.resolved_name)
 
         # Create ActionClient to the real server
@@ -69,7 +71,6 @@ class WrappedFJTA(object):
         rospy.loginfo("Created SimpleActionClient to " + '/' +
                       self.real_controller_name + '_controller/follow_joint_trajectory')
         self._ac.wait_for_server()
-
 
         self._as = actionlib.SimpleActionServer('/' + self.controller_name + '_controller/follow_joint_trajectory',
                                                 FollowJointTrajectoryAction,
@@ -128,11 +129,11 @@ class WrappedFJTA(object):
 
         # Create goal
         g = FollowJointTrajectoryGoal()
-        # We ignore path_tolerance and goal_tolerance... does not make sense in a gripper
+        # We ignore path_tolerance and goal_tolerance... does not make sense in
+        # a gripper
         g.goal_time_tolerance = goal.goal_time_tolerance
         jt = self.substitute_trajectory(goal.trajectory)
         g.trajectory = jt
-
 
         if not self._ac.wait_for_server(rospy.Duration(5.0)):
             rospy.logerr("Action server for gripper not found!")
@@ -143,6 +144,7 @@ class WrappedFJTA(object):
 
         # wait for goal to finish while checking if cancel is requested
         while not self._ac.wait_for_result(rospy.Duration(0.1)):
+            rospy.loginfo("Waiting for goal to finish...")
             # Deal with goal cancelled
             if self._as.is_preempt_requested():
                 self._ac.cancel_all_goals()
@@ -164,9 +166,10 @@ class WrappedFJTA(object):
 
     def pub_state(self, _):
         # Just repub the last state but with one joint stuff
-        st = self.state_msg(self._last_state, 'state')
+        if self._last_state:
+            st = self.state_msg(self._last_state, 'state')
 
-        self._state_pub.publish(st)
+            self._state_pub.publish(st)
 
     def state_msg(self, ref_state, type_msg):
         if type_msg == 'state':
@@ -176,22 +179,25 @@ class WrappedFJTA(object):
         st.joint_names = [self.gripper_joint_name]
         st.header = ref_state.header
 
-        st.desired.positions.extend(ref_state.desired.positions[:1] * 2.0)
+        st.desired.positions.extend(
+            [p * 2.0 for p in ref_state.desired.positions[:1]])
         st.desired.velocities.extend(ref_state.desired.velocities[:1])
         st.desired.accelerations.extend(ref_state.desired.accelerations[:1])
         st.desired.effort.extend(ref_state.desired.effort[:1])
         st.desired.time_from_start = ref_state.desired.time_from_start
 
-        st.actual.positions.extend(ref_state.actual.positions[:1] * 2.0)
+        st.actual.positions.extend(
+            [p * 2.0for p in ref_state.actual.positions[:1]])
         st.actual.velocities.extend(ref_state.actual.velocities[:1])
         st.actual.accelerations.extend(ref_state.actual.accelerations[:1])
         st.actual.effort.extend(ref_state.actual.effort[:1])
         st.actual.time_from_start = ref_state.actual.time_from_start
 
-        st.error.positions.extend(ref_state.error.positions[:1] * 2.0)
-        st.error.velocities.extend(ref_state.error.velocities[:1])
-        st.error.accelerations.extend(ref_state.error.accelerations[:1])
-        st.error.effort.extend(ref_state.error.effort[:1])
+        st.error.positions.extend(
+            [p * 2.0 for p in ref_state.error.positions[:1]])
+        st.error.velocities.extend(ref_state.error.velocities[: 1])
+        st.error.accelerations.extend(ref_state.error.accelerations[: 1])
+        st.error.effort.extend(ref_state.error.effort[: 1])
 
         return st
 
